46a28651ca878f96ae408c0331c30f61dca2f70b
AnaLuizanc <analuizanc13@gmail.com>
2026-05-07 16:25:32 -0300

    feat: implement header component with styling and update app structure

---
+++ src/app/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatIconModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  protected title = 'Welcome to the Home Page!';
+}
